634c9f5341aabff88c726a0e378cf9c30075258e
Anand Pandey <ap9012928@gmail.com>
2026-01-24 11:04:25 +0530

    modified models.py and admin.py

--- project_Django/project1/admin.py
+++ project_Django/project1/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import projectVariety
+from .models import projectVariety,ProjectReview,Store,ProjectCertificate
 # Register your models here.
-admin.site.register(projectVariety)
+
+class ProjectReviewInline(admin.TabularInline):
+    model=ProjectReview
+    extra=2
+    
+class projectVarietyAdmin(admin.ModelAdmin):
+    list_display=('name', 'type', 'date_added')
+    inlines = [ProjectReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display= ('name', 'location')
+    filter_horizontal= ('project_varieties',)
+    
+class ProjectCertificateAdmin(admin.ModelAdmin):
+    list_display= ('project', 'certificate_number')
+    
+admin.site.register(projectVariety, projectVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ProjectCertificate, ProjectCertificateAdmin)
